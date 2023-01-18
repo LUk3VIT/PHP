470d540429e962e5ae0f5361a8a72144025c9d18
LUk3VIT <lucasdiass061@gmail.com>
2023-01-18 13:26:39 -0300

    recapitulando 18%

---
+++ manipulando coleçoes com arrays/aula.php
@@ -0,0 +1,13 @@
+<?php
+
+$array = [
+    1 = 'um',
+    2 = 'dois',
+    3 = 'tres'
+];
+
+foreach ($array as $numeral => $nomeNumero){
+    echo "$numeral numeral emportugues é: "$nomeNumero . PHP_EOL
+} 
+
+echo "Total: ". count($array) . PHP_EOL
