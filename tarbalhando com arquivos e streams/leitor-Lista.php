34fcc7af8d936d468abb9bcb6d308c6274d0f3ef
LUk3VIT <lucasdiass061@gmail.com>
2023-02-06 11:46:38 -0300

    escrevendo em arquivos e wrappers e filtros 46%

--- tarbalhando com arquivos e streams/leitor-Lista.php
+++ tarbalhando com arquivos e streams/leitor-Lista.php
@@ -1,26 +1,35 @@
 <?php
-
+/*
 //aqui nós ta pegando abrindo um arquivo
 $arquivo = fopen('lista.txt', 'r') ;
+*/
 
 /*
-nesse loop falamos, que emquanto tem linha leia e quando acabar sai do loop
+nesse loop falamos, que emquanto tem linha, leia e quando acabar, sai do loop:
+
 while (!feof($arquivo)){
     $nome = fgets($arquivo, 3);
 
     echo $nome;
 }
+
 */
 
-//pegando o tamanho do arquivo, para isso tem que ser o nome do arquivo e não a vaiavel
+/*
+
+//pegando o tamanho do arquivo, para isso tem que ser o nome do arquivo e não a variavel
 $tamanhoDoArquivo = filesize('lista.txt');
 
-//aqui serve para ler o arquivo em intiro pegando byte por byte
+
+//aqui serve para ler o arquivo inteiro pegando byte por byte
 $nome = fread ($arquivo, $tamanhoDoArquivo);
 echo $nome;
 
+
 //aqui nós fecha o arquivo que abrimos
 fclose($arquivo);
+
+*/
 
 //aqui o arquivo ja é aberto,lido e fechado 
 $nome = file_get_contents('lista.txt');
